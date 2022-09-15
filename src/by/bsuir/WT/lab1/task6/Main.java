b09c54cd674a063c8bb622d8f56f882d6aa036f4
egorpugachevskyWork <egorpugachevsky@gmail.com>
2022-09-15 22:53:23 +0300

    Type of Task 6 was fixed

--- src/by/bsuir/WT/lab1/task6/Main.java
+++ src/by/bsuir/WT/lab1/task6/Main.java
@@ -1,21 +1,25 @@
 package by.bsuir.WT.lab1.task6;
 
-import by.bsuir.WT.lab1.task6.data.IntegerArrayForTask6;
+import by.bsuir.WT.lab1.task6.data.DoubleArrayForTask6;
 import by.bsuir.WT.lab1.task6.logic.Task6;
 
+import java.text.DecimalFormat;
+
 public class Main {
+    private static final DecimalFormat df = new DecimalFormat("0.0");
+
     public static void main(String[] args){
-        int[] numbers = IntegerArrayForTask6.getArray();
-        int size = IntegerArrayForTask6.getSize();
-        int[][] squareMatrix = Task6.makeSquareMatrix(numbers, size);
+        double[] numbers = DoubleArrayForTask6.getArray();
+        int size = DoubleArrayForTask6.getSize();
+        double[][] squareMatrix = Task6.makeSquareMatrix(numbers, size);
         DisplayMatrix(squareMatrix, size);
     }
 
-    private static void DisplayMatrix(int[][] matrix, int size) {
+    private static void DisplayMatrix(double[][] matrix, int size) {
         System.out.println("Received matrix: ");
         for (int i = 0; i < size; ++i){
             for (int j = 0; j < size; ++j){
-                System.out.printf("%6d ", matrix[i][j]);
+                System.out.printf("%6s ", df.format(matrix[i][j]));
             }
             System.out.println();
         }
--- src/by/bsuir/WT/lab1/task6/logic/Task6.java
+++ src/by/bsuir/WT/lab1/task6/logic/Task6.java
@@ -1,8 +1,8 @@
 package by.bsuir.WT.lab1.task6.logic;
 
 public class Task6 {
-    public static int[][] makeSquareMatrix(int[] numbers, int size){
-        int[][] matrix = new int[size][];
+    public static double[][] makeSquareMatrix(double[] numbers, int size){
+        double[][] matrix = new double[size][];
         for (int i = 0; i < size; ++i){
             matrix[i] = getShiftedArray(numbers, size, i);
         }
@@ -10,8 +10,8 @@ public class Task6 {
         return matrix;
     }
 
-    private static int[] getShiftedArray(int[] numbers, int size, int shiftDegree){
-        int[] newNumbers = new int[size];
+    private static double[] getShiftedArray(double[] numbers, int size, int shiftDegree){
+        double[] newNumbers = new double[size];
         for (int i = 0; i < size; ++i) {
             newNumbers[i] =  numbers[(i + shiftDegree) % size];
         }
